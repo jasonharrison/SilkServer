526c76677439d897958df62f4e284561f09a97ab
Jason <jason@connectgate.net>
2013-01-22 21:01:45 -0500

    Replaced requests.get with self.request

--- client.py
+++ client.py
@@ -8,7 +8,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import requests,json
+import requests
 
 API_URI = "https://api.digitalocean.com/"
 
@@ -27,23 +27,23 @@ class Droplet:
     def create(self, name, size_num, image_id, region_id): #size_num != size id.  the higher the number, the bigger the server.  image ids: 12573 is debian x64. region ids: 1 = new york 1, 2 = amsterdam 1.  
         sizes=requests.get(API_URI+"sizes/",params={"client_id": self.client_id, "api_key": self.api_key}).json().get("sizes")
         size_id=sizes[size_num]['id']
-        r=self.request("new",payload={"name":name,"size_id":size_id,'image_id':image_id,'region_id':region_id})
+        r=self.request("droplets/new",payload={"name":name,"size_id":size_id,'image_id':image_id,'region_id':region_id})
         self.droplet_id = r.json()['droplet']['id']
     def destroy(self):
         if self.droplet_id == None:
             return False
-        r=self.request(str(self.droplet_id)+"/destroy")
+        self.request("droplets/"+str(self.droplet_id)+"/destroy")
         return True
     def info(self):
         if self.droplet_id == None:
             return False
-        droplet_info=requests.get(API_URI+"droplets/"+str(self.droplet_id),params={"client_id": self.client_id, "api_key": self.api_key}).json()['droplet']
+        droplet_info=self.request("droplets/"+str(self.droplet_id),payload={"client_id": self.client_id, "api_key": self.api_key}).json()['droplet']
         return droplet_info
     def request(self, target, payload={}, action="servers"):
         headers = {'User-Agent': 'SilkServer/git'}
         payload['client_id'] = self.client_id
         payload['api_key'] = self.api_key
-        r = requests.get(API_URI+"/droplets/"+target+"/", headers=headers, params=payload)
+        r = requests.get(API_URI+target+"/", headers=headers, params=payload)
         print r.json()
         assert r.json().get("status") != "ERROR"
         return r
